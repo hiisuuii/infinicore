2f80e9027e59097f68cd04e3142b5b0b4c24e944
hiisuuii <dogglez7777@gmail.com>
2023-09-29 17:43:53 -0600

    Increase level cost for applying

--- src/main/java/hisui/infinicore/mixin/InfinicoreMixin.java
+++ src/main/java/hisui/infinicore/mixin/InfinicoreMixin.java
@@ -24,20 +24,19 @@ abstract class InfinicoreMixin extends ForgingScreenHandler {
 		super(type, syncId, playerInventory, context);
 	}
 
-	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamageable()Z", ordinal = 0),
-			method = "updateResult", cancellable = true)
+	@Inject(method = "updateResult",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamageable()Z", ordinal=0), cancellable = true)
 	private void inject(CallbackInfo info) {
 		ItemStack itemStack1 = this.input.getStack(0).copy();
-		this.levelCost.set(10);
 		ItemStack itemStack2 = this.input.getStack(1);
 		if (itemStack1.isDamageable() && itemStack2.isOf(Main.INFINICORE)) {
-			NbtCompound nbt = itemStack1.getOrCreateNbt();
+			this.levelCost.set(10);
+			NbtCompound nbt = itemStack1.getNbt();
 			nbt.putBoolean("Unbreakable", true);
 			itemStack1.setNbt(nbt);
 			itemStack1.setDamage(0);
 
 			this.output.setStack(0, itemStack1);
-			this.levelCost.set(0);
 			sendContentUpdates();
 			info.cancel();
 		}
